e64f342a05c1678d87b535a8d79b2a5715ed3463
Jeremy Adams <jeremy@dagger.io>
2024-06-27 12:17:20 -0700

    working
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- dagger/src/index.ts
+++ dagger/src/index.ts
@@ -1,41 +1,44 @@
 /**
- * A generated module for Testgit functions
- *
- * This module has been generated via dagger init and serves as a reference to
- * basic module structure as you get started with Dagger.
- *
- * Two functions have been pre-created. You can modify, delete, or add to them,
- * as needed. They demonstrate usage of arguments and return types using simple
- * echo and grep commands. The functions can be called from the dagger CLI or
- * from one of the SDKs.
- *
- * The first line in this comment block is a short description line and the
- * rest is a long description with more detail on the module's purpose or usage,
- * if appropriate. All modules should have a short description.
+ * Testgit
  */
 import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
 
 @object()
 class Testgit {
-  /**
-   * Returns a container that echoes whatever string argument is provided
-   */
-  @func()
-  containerEcho(stringArg: string): Container {
-    return dag.container().from("alpine:latest").withExec(["echo", stringArg])
-  }
+    /**
+     * Returns a Container read to ls -al the /src dir from git repo
+     */
+    @func()
+    async gitLs(repoDir?: Directory, repoStr?: string): Promise<Container> {
+        var ctr:Container = dag.container().from("alpine:latest")
 
-  /**
-   * Returns lines that match a pattern in the files of the provided Directory
-   */
-  @func()
-  async grepDir(directoryArg: Directory, pattern: string): Promise<string> {
-    return dag
-      .container()
-      .from("alpine:latest")
-      .withMountedDirectory("/mnt", directoryArg)
-      .withWorkdir("/mnt")
-      .withExec(["grep", "-R", pattern, "."])
-      .stdout()
-  }
+        if ((repoDir && repoStr) || (!repoDir && !repoStr)) {
+            throw new Error("Provide exactly one arg. Either `repo-dir` or `repo-str`.")
+        }
+        if (repoDir) {
+            ctr = ctr
+            .withDirectory("/src", repoDir)
+        }
+        if (repoStr) {
+            try {
+                console.log("Trying branch `main`...")
+                ctr = await ctr
+                .withDirectory("/src", dag.git(repoStr).branch("main").tree())
+                .sync()
+            } catch (error) {
+                console.error("No `main` branch...");
+                try {
+                    console.log("Trying branch `master`...")
+                    ctr = await ctr
+                    .withDirectory("/src", dag.git(repoStr).branch("master").tree())
+                    .sync()
+                } catch (error) {
+                    throw new Error("Repo must have either `main` or `master` branch.")
+                }
+            }
+        }
+        return ctr
+        .withWorkdir("/src")
+        .withExec(["ls", "-alh"])
+    }
 }
